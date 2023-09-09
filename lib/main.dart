45423e397d684d2990f79b61d8d06a9a75163a7a
tarakojo <tarakojo1019@gmail.com>
2023-09-09 15:14:15 +0900

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,18 @@
 import 'dart:async';
+import 'package:amplify_authenticator/amplify_authenticator.dart';
 import 'package:flutter/material.dart';
 import 'package:sealchat/ui/testchat.dart';
+import 'package:amplify_flutter/amplify_flutter.dart';
+import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
 import './ui/sealview.dart';
+import './amplifyconfiguration.dart';
 
 Future main() async {
- 
   await initSealView();
-  runApp(MaterialApp(home: new MyApp()));
+  runApp(MaterialApp(initialRoute: '/', routes: <String, WidgetBuilder>{
+    '/': (context) => MyApp(),
+    '/chat': (context) => MyApp2(),
+  }));
 }
 
 class MyApp extends StatefulWidget {
@@ -18,7 +24,41 @@ class _MyAppState extends State<MyApp> {
   @override
   void initState() {
     super.initState();
+    _configureAmplify();
   }
+
+  Future<void> _configureAmplify() async {
+    try {
+      final auth = AmplifyAuthCognito();
+      await Amplify.addPlugin(auth);
+
+      // call Amplify.configure to use the initialized categories in your app
+      await Amplify.configure(amplifyconfig);
+    } on Exception catch (e) {
+      safePrint('An error occurred configuring Amplify: $e');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Authenticator(
+        child: MaterialApp(
+            builder: Authenticator.builder(),
+            home: Scaffold(
+                appBar: AppBar(title: Text("Official InAppWebView website")),
+                body: SafeArea(
+                    child: Column(children: <Widget>[
+                  Expanded(
+                    child: Stack(children: [
+                      ChatRoom(),
+                    ]),
+                  )
+                ])))));
+  }
+}
+
+class MyApp2 extends StatelessWidget {
+  const MyApp2({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -28,7 +68,7 @@ class _MyAppState extends State<MyApp> {
             child: Column(children: <Widget>[
           Expanded(
             child: Stack(children: [
-              ChatRoom(),
+              SealView(),
             ]),
           )
         ])));
