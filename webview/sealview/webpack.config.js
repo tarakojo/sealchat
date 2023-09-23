1c127026456eefbc51cd99113f88c51a3090472c
tarakojo <tarakojo1019@gmail.com>
2023-09-23 18:23:28 +0900

    updated

--- webview/sealview/webpack.config.js
+++ webview/sealview/webpack.config.js
@@ -1,34 +1,22 @@
 const CopyPlugin = require("copy-webpack-plugin");
-var path = require('path');
+const path = require('path');
 
 module.exports = {
-  mode: 'production',
-  target: ['web', 'es5'],
-  entry: ['./src/main.ts'],
+  entry: './src/index.js',  
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/dist/'
-  },
-  resolve: {
-    extensions: ['.ts', '.js'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        exclude: /node_modules/,
-        loader: 'ts-loader'
-      }
-    ]
+    filename: 'bundle.js', 
+    path: path.resolve(__dirname, 'dist'), 
   },
   plugins: [
-    new CopyPlugin({
-      patterns : [
-        {from:"./CubismSdkForWeb/Core/live2dcubismcore.min.js", to : "./live2dcubismcore.min.js"},
-        {from:"./src/index.html", to : "./index.html"},
-        {from:"./assets", to : "./assets"},
-      ]
-    })
-  ]
-}
+  new CopyPlugin({
+    patterns : [
+      {from:"./CubismSdkForWeb/Core/live2dcubismcore.min.js", to : "./scripts/live2d/Core/live2dcubismcore.min.js"},
+      {from:"./CubismSdkForWeb/Framework/dist", to : "./scripts/live2d/Framework", filter: (resourcePath) => {
+        return resourcePath.endsWith(".js");
+      }},
+      {from:"./Live2DRender.js", to : "./scripts/live2d/Live2DRender.js" },
+      {from:"./src/index.html", to : "./index.html"},
+      {from:"./assets", to : "./assets"},
+    ]
+  })]
+};
