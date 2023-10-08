9e988f9ac8e0a7d7ba492e41ea0891315946d1de
tarakojo <tarakojo1019@gmail.com>
2023-10-08 22:49:11 +0900

    updated

--- functions/main.py
+++ functions/main.py
@@ -5,15 +5,19 @@ load_dotenv()
 # firebaseの初期化
 from firebase_functions import https_fn
 from firebase_admin import initialize_app
-initialize_app()
+from firebase_admin import firestore
+
+initialize_app() 
+db = firestore.client()
 
 import chat
-
+import database.database as database 
 
 REGION = "asia-northeast1"
 
 @https_fn.on_call(region=REGION)
 def seal_response(req: https_fn.Request):
+    database.get_aboutyou(db, req.auth.uid)
     chat_response = chat.chat(system_message="please anser yes or no", message=req.data["message"])
     return {
         "message": chat_response
