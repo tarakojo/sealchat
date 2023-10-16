dac37da2b95ed034c04c3d5479b19889b65c1c39
tarakojo <tarakojo1019@gmail.com>
2023-10-16 13:23:55 +0900

    deploy-version

--- functions/main.py
+++ functions/main.py
@@ -14,8 +14,6 @@ from firebase_admin import initialize_app
 from firebase_admin import firestore
 
 initialize_app() 
-db = firestore.client()
-
 import database as database 
 import chat as chat
 
@@ -24,30 +22,44 @@ REGION = "asia-northeast1"
 #新規アカウントは初めにこれを呼び出すこと。複数回呼び出しても問題ない。
 @https_fn.on_call(region=REGION)
 def init_user(req: https_fn.Request):
+    db = firestore.client()
+
     return database.init_user(db, req.auth.uid, req.data["info"])
 
 @https_fn.on_call(region=REGION)
 def get_info(req: https_fn.Request):
+    db = firestore.client()
+
     return database.get_info(db, req.auth.uid)
 
 @https_fn.on_call(region=REGION)
 def set_info(req: https_fn.Request):
+    db = firestore.client()
+
     return database.set_info(db, req.auth.uid, req.data["info"])
 
 @https_fn.on_call(region=REGION)
 def get_chat_history(req: https_fn.Request):
+    db = firestore.client()
+
     return database.get_chat_history(db, req.auth.uid)
 
 @https_fn.on_call(region=REGION)
 def get_calendar(req: https_fn.Request):
+    db = firestore.client()
+
     return database.get_calendar(db, req.auth.uid, req.data["startUnixTime"], req.data["endUnixTime"])
 
 @https_fn.on_call(region=REGION)
 def chat_without_message(req: https_fn.Request):
+    db = firestore.client()
+
     return chat.chat_without_input(db, req.auth.uid)
 
 @https_fn.on_call(region=REGION)
 def chat_with_message(req: https_fn.Request):
+    db = firestore.client()
+
     return chat.chat_with_input(db, req.auth.uid,req.data["currentUnixTime"], req.data["message"])
  
  
